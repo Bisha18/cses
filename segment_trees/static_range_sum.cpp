bfc6c5d5569204d9614f59a6075abd33638af9dc
bishal <d.bishalpaul@gmail.com>
2025-08-04 15:01:55 +0530

    static_range_sum

---
+++ segment_trees/static_range_sum.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MX = 1e5+1;
+int v[MX],seg[MX*4];
+
+void build(int i, int l, int r){
+   if(l==r){
+    seg[i]=v[l];
+    return;
+   }
+
+   int m = (l+r)/2;
+   build(2*i+1,l,m);
+   build(2*i+2,m+1,r);
+   seg[i] = seg[2*i+1]+seg[2*i+2];
+}
+
+int query(int i , int l, int r, int s, int e){
+  if(l>e || r<s) return 0;
+  if(l>=s && r<=e) return seg[i];
+  int m = (l+r)/2;
+  return query(2*i+1,l,m,s,e)+query(2*i+2,m+1,r,s,e);
+}
+
+
+
+signed main(){
+   int n,m; cin>>n>>m;
+   for(int i=0;i<n; i++) cin>>v[i];
+   build(0,0,n-1);
+   while(m--){
+    int a,b; cin>>a>>b;
+    a--,b--;
+    cout<<query(0,0,n-1,a,b)<<endl;
+   }
+}
